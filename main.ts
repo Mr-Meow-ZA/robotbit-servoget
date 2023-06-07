6958db7514498b1a8fb8c68b973790dc365e397a
Mr-Meow-ZA <108538294+Mr-Meow-ZA@users.noreply.github.com>
2023-06-07 16:10:09 +0200

    Update pxt.json, main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,30 @@
+// Servo Channel.
+enum Servos {
+    S1 = 0x01,
+    S2 = 0x02,
+    S3 = 0x03,
+    S4 = 0x04,
+    S5 = 0x05,
+    S6 = 0x06,
+    S7 = 0x07,
+    S8 = 0x08
+};
 
+
+namespace robotbitArm
+{
+    //% block="get current position for servo %servo"
+    export function i2creadCustom(servo: Servos, reg:number):number {
+        pins.i2cWriteNumber(servo, reg, NumberFormat.UInt8BE);
+        let val = pins.i2cReadNumber(servo, NumberFormat.UInt8BE);
+        return val;
+    }
+
+    function i2cwrite(addr: number, reg: number, value: number) {
+        let buf = pins.createBuffer(2)
+        buf[0] = reg
+        buf[1] = value
+        pins.i2cWriteBuffer(addr, buf)
+    }
+
+}
